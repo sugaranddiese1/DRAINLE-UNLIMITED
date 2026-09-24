d67eaf7ae91413d1cfb624406dbd60338ea3a8fb
sugaranddiese1 <ahmadkattank@gmail.com>
2026-09-24 19:08:27 -0400

    Configure Supabase multiplayer connection

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-window.BLAIDLE_SUPABASE={
-  url:"",
-  anonKey:""
+window.BLAIDLE_SUPABASE = {
+  url: "https://kmiscwbjpdyzjxdcplat.supabase.co",
+  anonKey: "sb_publishable_aoV66FsugCr7qx7qrUZWUQ_5jup2GQR"
 };
